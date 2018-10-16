e871bc2b9fdaab5a8d17eea16b91e42509138785
mike011 <mike_ch_1@hotmail.com>
2018-10-15 21:05:38 -0400

    example crashing

---
+++ DoTheUITestsWork/Pods/Target Support Files/Pods-DoTheUITestsWork/Pods-DoTheUITestsWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DoTheUITestsWork : NSObject
+@end
+@implementation PodsDummy_Pods_DoTheUITestsWork
+@end
---
+++ DoTheUITestsWork/Pods/Target Support Files/Pods-DoTheUITestsWorkTests/Pods-DoTheUITestsWorkTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DoTheUITestsWorkTests : NSObject
+@end
+@implementation PodsDummy_Pods_DoTheUITestsWorkTests
+@end
